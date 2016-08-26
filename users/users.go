5311154f68d85a164261e864587812cf1298f607
Jason Smith <jasonrichardsmith@gmail.com>
2016-08-26 11:16:01 +0200

    removed addresses and cards from user, spring doe not like extra json outside what it defined to expect

--- users/users.go
+++ users/users.go
@@ -17,11 +17,11 @@ var (
 type User struct {
 	FirstName string    `json:"firstName" bson:"firstName"`
 	LastName  string    `json:"lastName" bson:"lastName"`
-	Email     string    `json:"email" bson:"email"`
+	Email     string    `json:"-" bson:"email"`
 	Username  string    `json:"username" bson:"username"`
 	Password  string    `json:"-" bson:"password,omitempty"`
-	Addresses []Address `json:"addresses,omitempty" bson:"-"`
-	Cards     []Card    `json:"cards,omitempty" bson:"-"`
+	Addresses []Address `json:"-,omitempty" bson:"-"`
+	Cards     []Card    `json:"-,omitempty" bson:"-"`
 	UserID    string    `json:"id" bson:"-"`
 	Links     Links     `json:"_links"`
 	Salt      string    `json:"-" bson:"salt"`
